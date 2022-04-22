c5a5eb012c08701cdf87a397295e6116214882e9
Ridel Oscar García Mora <ridel.garcia@ntsprint.com>
2022-04-22 07:56:44 -0400

    initial commit

---
+++ src/test/java/com/ce/instashare/InstashareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ce.instashare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstashareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
